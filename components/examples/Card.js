f08836e1713b0f532fab02fb3003d9ff5a06474a
Anastasiia Filimonova <2850025@gmail.com>
2021-03-05 07:43:17 +0300

    examplesLayout

---
+++ components/examples/Card.js
@@ -0,0 +1,18 @@
+const Card = (props) => {
+    return (
+        <div className="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div className="card-body d-flex flex-column align-items-start">
+                <strong className="d-inline-block mb-2 text-primary">World</strong>
+                <h3 className="mb-0">
+                    <a className="text-dark" href="#">Featured post</a>
+                </h3>
+                <div className="mb-1 text-muted">Nov 12</div>
+                <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                <a href="#">Continue reading</a>
+            </div>
+            <img className="card-img-right flex-auto d-none d-lg-block" src="/img/examples/thumbnail.svg" alt="Card image cap" />
+        </div>
+    )
+}
+
+export default Card
--- pages/examples/blog.js
+++ pages/examples/blog.js
@@ -1,12 +1,23 @@
 import BlogLayout from "../../components/examples/BlogLayout"
 import Main from "../../components/examples/Main"
+import Card from "../../components/examples/Card"
 
 const BlogPage = () => {
-    return (
-      <BlogLayout>
-        <Main />
-      </BlogLayout>
-    )
+  return (
+    <BlogLayout>
+      <div className="container">
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <Card />
+          </div>
+          <div className="col-md-6">
+            <Card />
+          </div>
+        </div>
+      </div>
+      <Main />
+    </BlogLayout>
+  )
 }
 
 export default BlogPage
